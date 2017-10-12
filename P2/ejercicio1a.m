264a948f4474ce389a5bf0d754363ed695a71951
Daniel Perdices <daniel.perdices@estudiante.uam.es>
2017-10-12 10:59:27 +0200

    p2: 1a

--- P2/ejercicio1a.m
+++ P2/ejercicio1a.m
@@ -18,10 +18,15 @@ for c=1:3
     
     subplot(1,3,c);
     imagesc((diff(:,:,c)));
+    title(sprintf('Canal %c, E=%g', c, getEnergia(diff(:,:,c))))
 end
 
 figure
 subplot(1,2,1);
 imshow(ima);
+title(sprintf('Imagen original E=%g', getEnergia(ima)))
+
 subplot(1,2,2);
 imshow(uint8(ima_res));
+title(sprintf('Imagen resultado E=%g', getEnergia(ima_res)))
+test_ej_1_a(ima,ima_res,0)
